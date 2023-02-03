5e5d55fd89715e1c6bae333995d784321180f362
Sooyong Lee <sooyongl09@utexas.edu>
2023-02-03 16:11:26 -0600

    Update test.r

--- test/test.r
+++ test/test.r
@@ -1,7 +1,9 @@
-for(i in fs::dir_ls("R")){ source(i)}
-library(lavaan)
+# for(i in fs::dir_ls("R")){ source(i)}
+
 library(tidyverse)
-library(mvtnorm)
+library(clavaan)
+library(lavaan)
+
 unc.data <- lavaan::simulateData('
                      I =~ 1*y1 + 1*y2 + 1*y3 + 1*y4 + 1*y5
                      S =~ 0*y1 + 1*y2 + 2*y3 + 3*y4 + 4*y5
@@ -17,12 +19,12 @@ unc.data <- lavaan::simulateData('
 
 censored_prop <- c(left = 0.2, right = 0.8)
 
-cen_points <- cenPoint(x = unc.data,
+cen_points <- clavaan:::cenPoint(x = unc.data,
                        lower_quant = censored_prop[1],
                        upper_quant = censored_prop[2])
 
 
-cen.data <- cenData(unc.data, cen_points) %>%
+cen.data <- clavaan:::cenData(unc.data, cen_points) %>%
   data.frame() %>%
   set_names(names(unc.data))
 
@@ -42,10 +44,11 @@ cgrowth(model,
   model.type = 'growth') %>%
   summary()
 
-growth(model,
+lavaan::growth(model,
         data =cen.data) %>%
   summary()
 
-growth(model,
+lavaan::growth(model,
        data = unc.data) %>%
   summary()
+
